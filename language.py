57ace46513d5deb6d49cb135e21b7d32c01b8943
srujan1999 <mylaramsrujankumar@msitprogram.net>
2022-01-20 14:44:59 +0530

    Generate Sentences with Bigrams

--- language.py
+++ language.py
@@ -368,14 +368,14 @@ if __name__ == "__main__":
     # test.testBuildBigramProbs()
     # test.testGetTopWords()
     #test.testGenerateTextFromUnigrams()
-    #test.testGenerateTextFromBigrams()
+    test.testGenerateTextFromBigrams()
     
-
+"""
     print("\n" + "#"*15 + " WEEK 2 TESTS " +  "#" * 16 + "\n")
     test.week2Tests()
     print("\n" + "#"*15 + " WEEK 2 OUTPUT " + "#" * 15 + "\n")
     test.runWeek2()
-
+"""
     ## Uncomment these for Week 3 ##
 """
     print("\n" + "#"*15 + " WEEK 3 OUTPUT " + "#" * 15 + "\n")
